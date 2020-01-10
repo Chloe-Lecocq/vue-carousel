afab5cc5a3d62dacc625b18b1ea159000997b8f2
chloe.lecocq@etu.imt-lille-douai.fr <chloe.lecocq@davidson.fr>
2020-01-10 16:49:39 +0100

    Revert "no message"
    This reverts commit 89d6b6de5e911e30577c6824ed57d63614798fc6.

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -761,9 +761,7 @@ export default {
 
       // if it is a touch device, check if we are below the min swipe threshold
       // (if user scroll the page on the component)
-      if (this.isTouch && Math.abs(newOffsetX) < Math.abs(newOffsetY)) {
-        return;
-      }
+
 
       e.stopImmediatePropagation();
 
@@ -831,6 +829,10 @@ export default {
       // clamp the offset between 0 -> maxOffset
       this.offset = Math.max(0, Math.min(this.offset, this.maxOffset));
 
+      // update the current page
+      this.currentPage = this.scrollPerPage
+        ? Math.round(this.offset / this.slideWidth / this.currentPerPage)
+        : Math.round(this.offset / this.slideWidth);
     },
     /**
      * Re-compute the width of the carousel and its slides
