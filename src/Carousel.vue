90bb08feab26de67b18a13f0518d9a449c97306f
chloe.lecocq@etu.imt-lille-douai.fr <chloe.lecocq@davidson.fr>
2020-01-10 15:48:17 +0100

    no message

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -761,9 +761,7 @@ export default {
 
       // if it is a touch device, check if we are below the min swipe threshold
       // (if user scroll the page on the component)
-      if (this.isTouch && Math.abs(newOffsetX) < Math.abs(newOffsetY)) {
-        return;
-      }
+
 
       e.stopImmediatePropagation();
 
